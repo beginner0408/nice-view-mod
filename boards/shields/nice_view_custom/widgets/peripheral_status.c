cbf5e4d13ce64d74dac4adc14074573a484b69c5
beginner0408 <108817670+beginner0408@users.noreply.github.com>
2026-06-15 20:13:41 +0900

    Update peripheral_status.c

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -25,6 +25,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 LV_IMG_DECLARE(balloon);
 LV_IMG_DECLARE(mountain);
+LV_IMG_DECLARE(corro01);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
@@ -115,10 +116,18 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
     lv_obj_t *art = lv_img_create(widget->obj);
-    bool random = sys_rand32_get() & 1;
-    lv_img_set_src(art, random ? &balloon : &mountain);
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
+    //bool random = sys_rand32_get() & 1;
+    //lv_img_set_src(art, random ? &balloon : &mountain);
+    lv_img_set_src(art, &corro01);
 
+    //lv_obj_t * art = lv_animimg_create(widget->obj);            //<--
+    //lv_obj_center(art);                                         //<--
+    //lv_animimg_set_src(art, (const void **) anim_imgs, 12);     //<--
+    //lv_animimg_set_duration(art, CONFIG_CUSTOM_ANIMATION_SPEED);//<--
+    //lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);  //<--
+    //lv_animimg_start(art);                                      //<--
+
+    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
     widget_peripheral_status_init();
